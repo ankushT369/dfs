289a8c0b082cbcefe3620e1c147963424af49de1
Ankush Mondal <mondalankush9851@gmail.com>
2024-12-09 01:38:55 +0530

    started the implementation

--- include/dfs.h
+++ include/dfs.h
@@ -3,6 +3,22 @@
 
 #define FUSE_USE_VERSION 31
 
+#include <stdint.h>
+#include <stdlib.h>
+
+
+typedef struct file_mapping {
+    uint64_t uid;
+
+    const char* virtual_path;
+
+    size_t virtual_path_len;
+
+    const char* real_path;
+
+    size_t real_path_len;
+
+} f_map;
 
 
 
--- src/dfs.c
+++ src/dfs.c
@@ -4,14 +4,30 @@
 #include "/home/ankush/dfs/include/dfs.h"
 
 #include <fuse3/fuse.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
 
 
-//(const char *, struct stat *, struct fuse_file_info *fi)
+
+/**
+ *  @brief get the attributes of the directory or file
+ *
+ *  @param path The path is the path of the current 
+ *  directory or the file which is getting accessed
+ *
+ *  @param st The st is the pointer to the struct stat which
+ *  the information of the current directory or file
+ *
+ *  @param fi The fi is the pointer to the fuse_file_info 
+ *  currenty unused
+ *
+ */
 static int dfs_getattr(const char* path, struct stat* st, struct fuse_file_info* fi) {
-
 
     return 0;
 }
+
 
 struct fuse_operations ops = {
     .getattr = dfs_getattr,
